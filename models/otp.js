8d5c25dd92b210d165308248029ceb795cdbdbdb
AKHAZE-GODSWILL <onosetaleakhaze@gmail.com>
2022-09-13 15:29:09 +0100

    Changed the time the Otp expires on the data base to five minuites

--- models/otp.js
+++ models/otp.js
@@ -6,7 +6,7 @@ const otpSchema =  mongoose.Schema({
     otp: {type: String, required: true},
     expireAt: { type: Date,
                 default: Date.now,
-                index: { expires: 120}
+                index: { expires: 300}
     },
     
 }, {collection: 'otp'});
