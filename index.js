a907b07e2ff47858b545103319bfd554db3beadb
AKHAZE-GODSWILL <onosetaleakhaze@gmail.com>
2022-11-02 06:49:28 +0100

    Added the upload image endpoint

--- index.js
+++ index.js
@@ -40,11 +40,9 @@ app.use(bodyParser.urlencoded({
 
 app.use('/chats', require('./routes/chats'));
 app.use('/auth', require('./routes/auth'));
-// app.use('/sendImage', require('./utils/multer'));
 app.use('/viewUsers', require('./routes/viewOps'));
 app.use('/posts', require('./routes/posts'));
-// Made the uploads folder global so that I could access the contents of the folder from anywhere on through a URL
-app.use('/uploads', express.static('uploads'));
+
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT,()=> console.log(`Server running at port ${PORT}`));
